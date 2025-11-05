3e0b0b42f723ab562c7ecac93e53d2e82616d9b3
impalamol <=>
2025-11-05 14:04:20 +0530

    FEE-STRUCTURE-SCS-UPDATES

---
+++ fee-structure.php
@@ -0,0 +1,232 @@
+<?php
+$currentPage = 'fee-structure';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php include 'includes/styles.php'; ?>
+    <title>Barnes School & Junior College | Fee Structure</title>
+</head>
+
+<body>
+    <!-- <?php include 'includes/loader.php'; ?> -->
+    <?php include 'includes/header.php'; ?>
+
+    <main>
+        <section class="breadcrumbs" style="background: url(assets/media/breadcrumbs/about.jpg)">
+            <div class="breadcrumbContent">
+                <div class="container">
+                    <div class="row">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index"><i
+                                            class="ph-duotone ph-house-simple"></i></a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Fee Structure</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="feeStructure">
+            <div class="container">
+                <div class="row">
+                    <div class="wrapper flex-wrap flex-lg-nowrap">
+                        <div class="contentCard">
+                            <div class="content">
+                                <div class="pgHeader">
+                                    <div class="pgTitle">
+                                        <h2>Fee Structure: Boarding</h2>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>LKG to 4</h3>
+                                        <p class="subTitle">All Streams</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹297,550</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 5</h3>
+                                        <p class="subTitle">All Streams</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹297,550</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 6 to 8</h3>
+                                        <p class="subTitle">All Streams</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹297,550</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 9 &amp; 10 (Commerce)</h3>
+                                        <p class="subTitle">Commerce Stream</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹297,550</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 9 &amp; 10 (Science)</h3>
+                                        <p class="subTitle">Science Stream</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹297,550</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 11 Commerce or Art/Humanities</h3>
+                                        <p class="subTitle">Higher Secondary</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹2,54,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 11 Science</h3>
+                                        <p class="subTitle">Higher Secondary</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹2,54,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="content">
+                                <div class="pgHeader">
+                                    <div class="pgTitle">
+                                        <h2>Fee Structure: Day Scholar</h2>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Nursery</h3>
+                                        <p class="subTitle">Pre-Primary</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹58,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grades Kindergarten to 4</h3>
+                                        <p class="subTitle">All Streams</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹58,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 5</h3>
+                                        <p class="subTitle">All Streams</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹58,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grades 6 to 8</h3>
+                                        <p class="subTitle">All Streams</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹58,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grades 9 &amp; 10 (Commerce)</h3>
+                                        <p class="subTitle">Commerce Stream</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹58,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grades 9 &amp; 10 (Science)</h3>
+                                        <p class="subTitle">Science Stream</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹58,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 11 &amp; 12 Commerce or Art/Humanities</h3>
+                                        <p class="subTitle">Higher Secondary</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹65,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                                <div class="feeCard">
+                                    <div class="feeCardHeader">
+                                        <h3>Grade 11 &amp; 12 – Science</h3>
+                                        <p class="subTitle">Higher Secondary</p>
+                                    </div>
+                                    <div class="feeCardBody">
+                                        <span class="fee">₹65,750</span>
+                                        <small class="info">+ Other Applicable Fees</small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <?php include 'includes/sidebar.php'; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <div class="modal fade" id="highlightPopupModal" tabindex="-1" aria-labelledby="highlightPopupModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="highlightPopupModalLabel">Title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img src="assets/media/gallery/gallery.jpg" alt="Img Title" title="Img Title">
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'includes/footer.php'; ?>
+    <?php include 'includes/scripts.php'; ?>
+</body>
+
+</html>
